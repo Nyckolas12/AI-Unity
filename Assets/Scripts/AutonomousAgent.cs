87aaca2976f0980d39a14eacb60444500b714c0c
Nyckolas12 <nmendez@student.neumont.edu>
2025-01-16 11:16:43 -0700

    Spawner Agent

--- Assets/Scripts/AutonomousAgent.cs
+++ Assets/Scripts/AutonomousAgent.cs
@@ -6,6 +6,9 @@ public class AutonomousAgent : AIAgent
 
     private void Update()
     {
+        movement.ApplyForce(Vector3.forward * 10);
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-5, -5, -5), new Vector3(5, 5, 5));
+
         Debug.DrawRay(transform.position, transform.forward * perception.maxDistance, Color.yellow);
         var gameObjects = perception.GetGameObjects();
         foreach (var go in gameObjects)
